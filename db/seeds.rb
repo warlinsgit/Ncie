56f8b10b1eac3746412798408a8db3c608fc6ecc
warlinsgit <warlins25@gmail.com>
2018-12-14 14:47:29 +0000

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.create! :name => 'warley', :email => 'warlins25@hotmail.com', :password => 'varlei', :password_confirmation => 'varlei'
