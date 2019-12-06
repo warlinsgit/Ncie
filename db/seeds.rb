e547012a441ea59bf82f5eae2ab9b9dc3fe14e7b
warlinsgit <33092160+warlinsgit@users.noreply.github.com>
2019-12-06 09:16:19 +0000

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-AdminUser.create!(email: 'warlins18@hotmail.com', password: 'varlei', password_confirmation: 'varlei') if Rails.env.development?
+AdminUser.create!(email: '', password: '', password_confirmation: '') if Rails.env.development?
 
-AdminUser.create!(email: 'warlins18@hotmail.com', password: 'varlei', password_confirmation: 'varlei') 
+AdminUser.create!(email: '', password: '', password_confirmation: '') 
 
 
